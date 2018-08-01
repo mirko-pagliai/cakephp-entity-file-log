3d316627c4cb15c3a1171b665cd5a9300093b902
mirko-pagliai <mirko.pagliai@gmail.com>
2018-08-01 14:35:44 +0200

    first commit

---
+++ tests/TestCase/Log/Engine/EntityFileLogTest.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * This file is part of cakephp-entity-file-log.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-entity-file-log
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace EntityFileLog\Test\TestCase\Log\Engine;
+
+use Cake\Log\Log;
+use Cake\TestSuite\TestCase;
+use EntityFileLog\Log\Engine\EntityFileLog;
+use Tools\ReflectionTrait;
+use Tools\TestSuite\TestCaseTrait;
+
+/**
+ * EntityFileLogTest class
+ */
+class EntityFileLogTest extends TestCase
+{
+    use ReflectionTrait;
+    use TestCaseTrait;
+
+    /**
+     * Internal method to write some logs
+     */
+    protected function writeSomeLogs()
+    {
+        Log::error('This is an error message');
+        Log::critical('This is a critical message');
+    }
+
+    /**
+     * Teardown any static object changes and restore them
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        safe_unlink_recursive(LOGS);
+    }
+
+    /**
+     * Test for `getLogAsObject()` method
+     * @test
+     */
+    public function testGetLogAsObject()
+    {
+        $getLogAsObjectMethod = function () {
+            $object = new EntityFileLog;
+
+            return $this->invokeMethod($object, 'getLogAsObject', func_get_args());
+        };
+
+        $expectedAttributes = <<<ATTRIBUTES
+array (
+  'class' => 'NoExistingRoute',
+  'plugin' => false,
+  'prefix' => false,
+  '_ext' => false,
+)
+ATTRIBUTES;
+
+        $expectedTrace = <<<TRACE
+#0 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/ControllerFactory.php(72): Cake\Http\ControllerFactory->missingController(Object(Cake\Network\Request))
+#1 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/ActionDispatcher.php(92): Cake\Http\ControllerFactory->create(Object(Cake\Network\Request), Object(Cake\Network\Response))
+#2 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/BaseApplication.php(83): Cake\Http\ActionDispatcher->dispatch(Object(Cake\Network\Request), Object(Cake\Network\Response))
+#3 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/Runner.php(65): Cake\Http\BaseApplication->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response), Object(Cake\Http\Runner))
+#4 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Routing/Middleware/RoutingMiddleware.php(62): Cake\Http\Runner->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response))
+#5 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/Runner.php(65): Cake\Routing\Middleware\RoutingMiddleware->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response), Object(Cake\Http\Runner))
+#6 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Routing/Middleware/AssetMiddleware.php(88): Cake\Http\Runner->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response))
+#7 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/Runner.php(65): Cake\Routing\Middleware\AssetMiddleware->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response), Object(Cake\Http\Runner))
+#8 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Error/Middleware/ErrorHandlerMiddleware.php(81): Cake\Http\Runner->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response))
+#9 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/Runner.php(65): Cake\Error\Middleware\ErrorHandlerMiddleware->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response), Object(Cake\Http\Runner))
+#10 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/Runner.php(51): Cake\Http\Runner->__invoke(Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response))
+#11 /home/mirko/Server/mirkopagliai/vendor/cakephp/cakephp/src/Http/Server.php(90): Cake\Http\Runner->run(Object(Cake\Http\MiddlewareQueue), Object(Zend\Diactoros\ServerRequest), Object(Zend\Diactoros\Response))
+#12 /home/mirko/Server/mirkopagliai/webroot/index.php(37): Cake\Http\Server->run()
+#13 {main}
+TRACE;
+
+        $result = $getLogAsObjectMethod('error', 'example of message');
+        $this->assertTrue($result->has(['level', 'datetime', 'message', 'full']));
+        $this->assertEquals('error', $result->level);
+        $this->assertRegExp('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $result->datetime);
+        $this->assertEquals('example of message', $result->message);
+
+        $result = $getLogAsObjectMethod('error', file_get_contents(TESTS . 'examples' . DS . 'stacktrace1'));
+        $this->assertTrue($result->has(['level', 'datetime', 'exception', 'message', 'request', 'ip', 'trace', 'full']));
+        $this->assertEquals('error', $result->level);
+        $this->assertRegExp('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $result->datetime);
+        $this->assertEquals('Cake\Routing\Exception\MissingControllerException', $result->exception);
+        $this->assertEquals('Controller class NoExistingRoute could not be found.', $result->message);
+        $this->assertEquals('/noExistingRoute', $result->request);
+        $this->assertEquals('1.1.1.1', $result->ip);
+        $this->assertEquals($expectedTrace, $result->trace);
+
+        $result = $getLogAsObjectMethod('error', file_get_contents(TESTS . 'examples' . DS . 'stacktrace2'));
+        $this->assertTrue($result->has([
+            'level',
+            'datetime',
+            'exception',
+            'message',
+            'attributes',
+            'request',
+            'referer',
+            'ip',
+            'trace',
+            'full',
+        ]));
+        $this->assertEquals('error', $result->level);
+        $this->assertRegExp('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $result->datetime);
+        $this->assertEquals('Cake\Routing\Exception\MissingControllerException', $result->exception);
+        $this->assertEquals('Controller class NoExistingRoute could not be found.', $result->message);
+        $this->assertEquals($expectedAttributes, $result->attributes);
+        $this->assertEquals('/noExistingRoute', $result->request);
+        $this->assertEquals('/noExistingReferer', $result->referer);
+        $this->assertEquals('1.1.1.1', $result->ip);
+        $this->assertEquals($expectedTrace, $result->trace);
+    }
+
+    /**
+     * Test for `log()` method
+     * @test
+     */
+    public function testLog()
+    {
+        //Writes some logs
+        $this->writeSomeLogs();
+
+        $this->assertContains('Error: This is an error message', file_get_contents(LOGS . 'error.log'));
+        $this->assertContains('Critical: This is a critical message', file_get_contents(LOGS . 'error.log'));
+
+        $logs = safe_unserialize(file_get_contents(LOGS . 'error_serialized.log'));
+        $this->assertNotEmpty($logs);
+
+        foreach ($logs as $log) {
+            $this->assertInstanceOf('Cake\ORM\Entity', $log);
+            $this->assertContains($log->level, ['critical', 'error']);
+            $this->assertRegExp('/^\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}$/', $log->datetime);
+            $this->assertRegExp('/^This is (a critical|an error) message$/', $log->message);
+            $this->assertRegExp('/^[\d-:\s]{19} (Critical|Error)/', $log->full);
+        }
+
+        $this->assertFilePerms(LOGS . 'error.log', ['0644', '0664']);
+        $this->assertFilePerms(LOGS . 'error_serialized.log', ['0644', '0664']);
+
+        //Deletes all logs, drops and reconfigure adding `mask` option
+        safe_unlink_recursive(LOGS);
+        $oldConfig = Log::getConfig('error');
+        Log::drop('error');
+        Log::setConfig('error', $oldConfig + ['mask' => 0777]);
+
+        //Writes some logs
+        $this->writeSomeLogs();
+
+        $this->assertContains('Error: This is an error message', file_get_contents(LOGS . 'error.log'));
+        $this->assertContains('Critical: This is a critical message', file_get_contents(LOGS . 'error.log'));
+
+        $this->assertNotEmpty(file_get_contents(LOGS . 'error_serialized.log'));
+
+        $this->assertFilePerms(LOGS . 'error.log', '0777');
+        $this->assertFilePerms(LOGS . 'error_serialized.log', '0777');
+
+        Log::drop('error');
+        Log::setConfig('error', $oldConfig);
+    }
+
+    /**
+     * Test for `log()` method on failure
+     * @expectedException PHPUnit\Framework\Error\Warning
+     * @test
+     */
+    public function testLogOnFailure()
+    {
+        $SerializedLog = $this->getMockBuilder(EntityFileLog::class)
+            ->setConstructorArgs([['mask' => 0777, 'path' => LOGS]])
+            ->setMethods(['checkPermissionMask'])
+            ->getMock();
+
+        $SerializedLog->method('checkPermissionMask')
+            ->will($this->returnValue(false));
+
+        $SerializedLog->log('error', 'a message');
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of cakephp-entity-file-log.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/cakephp-entity-file-log
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+use Cake\Core\Plugin;
+use Cake\Log\Log;
+use EntityFileLog\Log\Engine\EntityFileLog;
+
+ini_set('intl.default_locale', 'en_US');
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+// Path constants to a few helpful things.
+define('ROOT', dirname(__DIR__) . DS);
+define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
+define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
+define('TESTS', ROOT . 'tests' . DS);
+define('TMP', sys_get_temp_dir() . DS . 'cakephp-entity-log' . DS);
+define('LOGS', TMP . 'logs' . DS);
+
+safe_mkdir(LOGS);
+
+require CORE_PATH . 'config' . DS . 'bootstrap.php';
+
+date_default_timezone_set('UTC');
+mb_internal_encoding('UTF-8');
+
+Plugin::load('EntityFileLog', ['bootstrap' => true, 'path' => ROOT]);
+
+Log::setConfig('error', [
+    'className' => EntityFileLog::class,
+    'path' => LOGS,
+    'file' => 'error',
+    'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+]);
+
+$_SERVER['PHP_SELF'] = '/';
